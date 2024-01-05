0168af9faecb40d2c4b4eff2b7975bf8b62d17f0
Nordgaren <nordgarentv@gmail.com>
2024-01-04 23:54:15 -0700

    update build test

--- build/src/tests.rs
+++ build/src/tests.rs
@@ -2,12 +2,8 @@ use crate::resource_builder::ResourceBuilder;
 #[test]
 #[should_panic]
 fn duplicate_panic() {
-    let dir = std::env::current_dir().unwrap();
-    let res = ResourceBuilder::new(dir.to_str().unwrap().to_string())
-        .add_xor_strs(&["string1", "string2"])
-        .add_xor_strs(&["string3"])
-        .add_xor_strs(&["duplicate string"])
-        .add_xor_strs(&["duplicate string"])
-        .add_xor_strings(&["string4".to_string(), "string5".to_string()]);
-    res.build();
+    ResourceBuilder::default()
+        .add_xor_resource("duplicate string")
+        .add_xor_resource("duplicate string")
+        .build();
 }
