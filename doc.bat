16dd1e61ec08b24c88f0dad266dfd492df71113c
Nordgaren <nordgarentv@gmail.com>
2024-01-10 16:39:55 -0700

    Add features to docs

--- doc.bat
+++ doc.bat
@@ -1,4 +1,4 @@
-cargo doc --no-deps -p "embre*"
+cargo doc --no-deps -p "embre*" --all-features
 rmdir /s ./docs
 robocopy target/doc docs /s
 echo|set /p="<meta http-equiv="refresh" content="0; url=embre/index.html">" > docs/index.html
