4025c64630fae6ff28603f0994678e5ec367b68c
Nordgaren <nordgarentv@gmail.com>
2024-01-10 14:43:03 -0700

    doc.bat update and oops update

--- doc.bat
+++ doc.bat
@@ -1,4 +1,4 @@
 cargo doc --no-deps -p "embre*"
 rmdir /s ./docs
 robocopy target/doc docs /s
-echo|set /p="<meta http-equiv="refresh" content="0; url=PROJECT_NAME/index.html">" > docs/index.html
+echo|set /p="<meta http-equiv="refresh" content="0; url=embre/index.html">" > docs/index.html
