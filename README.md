ec544e51a65cc441799d28eed04e4af9f22273f4
Nordgaren <nordgarentv@gmail.com>
2024-02-03 03:43:37 -0700

    Documentation update and add github link to docs

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # EmbRe - Embedded Resources API for Rust  
+[GitHub](https://github.com/Nordgaren/embre)  
 If GIF can be pronounced "JIF", then my jift to the world is that this crate is pronounced "Ember".
 A crate for encrypting, embedding and comparing encrypted resources to non encrypted resources, in Rust.
 
--- docs/src-files.js
+++ docs/src-files.js
@@ -1,10 +1,4 @@
 var srcIndex = JSON.parse('{\
-"embre":["",[["aes",[],["aes_data.rs","aes_resource.rs","aes_string.rs","mod.rs"]],["xor",[],["compare.rs","mod.rs","xor_data.rs","xor_resource.rs","xor_string.rs"]]],["embedded_resource.rs","lib.rs"]],\
-"embre_build":["",[["resource",[],["aes_resource.rs","mod.rs","plaintext_resource.rs","xor_resource.rs"]]],["config.rs","lib.rs","resource_builder.rs","util.rs"]],\
-"embre_core":["",[],["lib.rs","literal_bytes.rs","xor.rs"]],\
-"embre_crypt":["",[],["aes.rs","lib.rs","xor.rs"]],\
-"embre_macro":["",[],["lib.rs"]],\
-"embre_test":["",[["build",[],["aes.rs","mod.rs","xor.rs"]],["crypt",[],["aes.rs","mod.rs"]]],["consts.rs","lib.rs"]],\
 "embre_utils":["",[],["lib.rs"]]\
 }');
 createSrcSidebar();
